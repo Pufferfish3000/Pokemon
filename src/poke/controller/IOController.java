774cc5d8eb9bc90fd7765862020aca8a4e23b712
Braden Byrnes <mattbyrnes18@gmail.com>
2022-02-24 09:29:27 -0700

    Create IOController.java

---
+++ src/poke/controller/IOController.java
@@ -0,0 +1,22 @@
+package poke.controller;
+
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+import poke.model.monsters.Pokemon;
+import poke.view.PokeFrame;
+
+public class IOController 
+{
+	public static ArrayList<Pokemon> loadData()
+	{
+		ArrayList<Pokemon> pokeList = new ArrayList<Pokemon>();
+		
+		return pokeList;
+	}
+	
+	public static void saveData()
+	{
+		
+	}
+}
